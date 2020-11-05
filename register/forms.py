ab3eb88fc0249961747e11626cf7d2504dbf7281
Aaryamann171 <aaryamann171@gmail.com>
2020-11-06 02:52:04 +0530

    added authentication to pages, changed login/logout redirectly, bug fixes

--- register/forms.py
+++ register/forms.py
@@ -3,19 +3,20 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-    
+
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].label = "username"
         self.fields['username'].help_text = "alphanumerics and @/./+/-/_ only"
-        
+
         self.fields['email'].label = "email"
 
         self.fields['password1'].label = "password"
