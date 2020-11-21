4b4ea0bd4bcb623c99ad30e999a25f17afea3b0e
Akshit Sarin <akshitsarin99@gmail.com>
2020-11-22 00:21:57 +0530

    updated settings.py

--- price_pred/settings.py
+++ price_pred/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'ekj(97u@qg&%9dxqebs_ga)g=14koed133b*$78^wssg&vky7!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'predie.herokuapp.com']
 
 # Application definition
 
@@ -68,8 +68,7 @@ WSGI_APPLICATION = 'price_pred.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-# DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+db_from_env = dj_database_url.config(conn_max_age=600)
 
 DATABASES = {
     'default': {
@@ -77,6 +76,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
